492baea4e1f32be7e8e236d37e9794eb57a828b4
Jiri Kucera <jkucera@redhat.com>
2025-03-11 15:51:46 +0100

    WIP-backup

--- tests/files/vmutils.sh
+++ tests/files/vmutils.sh
@@ -170,3 +170,52 @@ function create_cloud_init_metadata_iso() (
         ./user-data \
         ./meta-data
 )
+
+function remote_sxx() {
+    local _sxx="${1:-}"
+    local _id_rsa="${2:-}"
+    declare -a _sxx_params=()
+
+    if [[ "${_sxx}" != ssh && "${_sxx}" != scp ]]; then
+        echo "$0: First argument must be either \`ssh\` or \`scp\`" >&2
+        return 1
+    fi
+    if [[ -z "${_id_rsa}" || ! -s "${_id_rsa}" ]]; then
+        echo "$0: Missing SSH private key file" >&2
+        return 1
+    fi
+
+    shift 2
+
+    _sxx_params+=( ${SXX_COMMON_ARGS:-} -o LogLevel=ERROR -i "${_id_rsa}" )
+    if [[ "${_sxx}" == ssh ]]; then
+        _sxx_params+=( -t )
+    fi
+
+    timeout ${SXX_TIMEOUT:-0} "${_sxx}" "${_sxx_params[@]}" "$@"
+}
+
+function remote_ssh() {
+    remote_sxx ssh "$@"
+}
+
+function remote_sshq() {
+    remote_ssh "$@" >/dev/null 2>&1
+}
+
+function remote_scp() {
+    remote_sxx scp "$@"
+}
+
+function discover_python_interpreter() {
+    local _python
+
+    for _item in python3 python /usr/libexec/platform-python; do
+        _python="$(${1:-} command -v "${_item}" 2>/dev/null)" || :
+        if [[ -n "${_python}" ]]; then
+            echo -n "${_python}"
+            return
+        fi
+    done
+    return 1
+}
